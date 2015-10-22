80fc1b45a26c58d6eeac4875ebbd0529ad9d54a7
Joe Winter <joe@joeventures.com>
2015-10-21 23:43:40 -0400

    Uploading what I have at this point. More to come!

--- lib/myhub/github.rb
+++ lib/myhub/github.rb
@@ -10,5 +10,21 @@ module Myhub
         "User-Agent"     => "HTTParty"
       }
     end
+
+    def list_repo_issues(owner, repo)
+      Github.get(
+          "/repos/#{owner}/#{repo}/issues",
+          :headers => @headers,
+          :query    => { state: "all", filter: "assigned", assignee: "Joeventures" }
+      )
+    end
+
+    def edit_issue_state(owner, repo, number, new_state)
+      Github.patch(
+          "/repos/#{owner}/#{repo}/issues/#{number}",
+          :headers => @headers,
+          :body    => { state: new_state }.to_json
+      )
+    end
   end
 end
